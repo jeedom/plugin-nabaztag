6a777cea3e3a9930e58a923055b6e3fb216c7626
Loïc <loic@jeedom.com>
2016-07-28 18:17:08 +0200

    Improve UI

--- desktop/php/nabaztag.php
+++ desktop/php/nabaztag.php
@@ -36,11 +36,11 @@ if (count($eqLogics) == 0) {
        <div class="eqLogicThumbnailContainer">
         <div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
             <center>
-             <i class="fa fa-plus-circle" style="font-size : 7em;color:#94ca02;"></i>
-         </center>
-         <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02"><center>Ajouter</center></span>
-     </div>
-     <?php
+               <i class="fa fa-plus-circle" style="font-size : 7em;color:#94ca02;"></i>
+           </center>
+           <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02"><center>Ajouter</center></span>
+       </div>
+       <?php
 foreach ($eqLogics as $eqLogic) {
 		$opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
 		echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
@@ -51,38 +51,48 @@ foreach ($eqLogics as $eqLogic) {
 		echo '</div>';
 	}
 	?>
-</div>
-<?php }
+  </div>
+  <?php }
 ?>
 </div>
 <div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
-    <form class="form-horizontal">
-        <fieldset>
-            <legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}<i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i></legend>
-            <div class="form-group">
-                <label class="col-sm-2 control-label">{{Nom de l'équipement Nabaztag}}</label>
-                <div class="col-sm-3">
-                    <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
-                    <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement Nabaztag}}"/>
+   <a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
+   <a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
+
+   <ul class="nav nav-tabs" role="tablist">
+    <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-tachometer"></i> {{Equipement}}</a></li>
+    <li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-list-alt"></i> {{Commandes}}</a></li>
+</ul>
+
+<div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
+    <div role="tabpanel" class="tab-pane active" id="eqlogictab">
+        <form class="form-horizontal">
+            <fieldset>
+                <legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}<i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i></legend>
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">{{Nom de l'équipement Nabaztag}}</label>
+                    <div class="col-sm-3">
+                        <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
+                        <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement Nabaztag}}"/>
+                    </div>
                 </div>
-            </div>
-            <div class="form-group">
-                <label class="col-sm-2 control-label" >{{Objet parent}}</label>
-                <div class="col-sm-3">
-                    <select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
-                        <option value="">{{Aucun}}</option>
-                        <?php
+                <div class="form-group">
+                    <label class="col-sm-2 control-label" >{{Objet parent}}</label>
+                    <div class="col-sm-3">
+                        <select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
+                            <option value="">{{Aucun}}</option>
+                            <?php
 foreach (object::all() as $object) {
 	echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
 }
 ?>
-                   </select>
+                       </select>
+                   </div>
                </div>
-           </div>
-           <div class="form-group">
-            <label class="col-sm-2 control-label">{{Catégorie}}</label>
-            <div class="col-sm-10">
-                <?php
+               <div class="form-group">
+                <label class="col-sm-2 control-label">{{Catégorie}}</label>
+                <div class="col-sm-10">
+                    <?php
 foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
 	echo '<label class="checkbox-inline">';
 	echo '<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '" />' . $value['name'];
@@ -90,58 +100,53 @@ foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
 }
 ?>
 
+               </div>
            </div>
-       </div>
-  <div class="form-group">
-        <label class="col-sm-2 control-label"></label>
-        <div class="col-sm-9">
-            <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
-            <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
+           <div class="form-group">
+            <label class="col-sm-2 control-label"></label>
+            <div class="col-sm-9">
+                <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
+                <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
+            </div>
         </div>
-    </div>
-   <div class="form-group">
-    <label class="col-sm-2 control-label">{{Adresse (openjabnab.fr ou @IP)}}</label>
-    <div class="col-sm-3">
-        <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="addr" placeholder="{{openjabnab.fr ou @IP}}"/>
-    </div>
-</div>
-<div class="form-group">
-    <label class="col-sm-2 control-label">{{Adresse MAC}}</label>
-    <div class="col-sm-3">
-        <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="mac" placeholder="{{Adresse MAC}}"/>
-    </div>
-</div>
-<div class="form-group">
-    <label class="col-sm-2 control-label">{{Token d'API Violet}}</label>
-    <div class="col-sm-3">
-        <input type="password" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="token" placeholder="{{Token}}"/>
-    </div>
-</div>
-</fieldset>
-</form>
-
-<legend>Commandes</legend>
-<table id="table_cmd" class="table table-bordered table-condensed">
-    <thead>
-        <tr>
-            <th>{{Nom}}</th>
-            <th>{{Paramètres}}</th>
-            <th>{{Action}}</th>
-        </tr>
-    </thead>
-    <tbody>
-
-    </tbody>
-</table>
-
-<form class="form-horizontal">
-    <fieldset>
-        <div class="form-actions">
-            <a class="btn btn-danger eqLogicAction" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
-            <a class="btn btn-success eqLogicAction" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
+        <div class="form-group">
+            <label class="col-sm-2 control-label">{{Adresse (openjabnab.fr ou @IP)}}</label>
+            <div class="col-sm-3">
+                <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="addr" placeholder="{{openjabnab.fr ou @IP}}"/>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-sm-2 control-label">{{Adresse MAC}}</label>
+            <div class="col-sm-3">
+                <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="mac" placeholder="{{Adresse MAC}}"/>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-sm-2 control-label">{{Token d'API Violet}}</label>
+            <div class="col-sm-3">
+                <input type="password" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="token" placeholder="{{Token}}"/>
+            </div>
         </div>
     </fieldset>
 </form>
+
+</div>
+<div role="tabpanel" class="tab-pane" id="commandtab">
+    <table id="table_cmd" class="table table-bordered table-condensed">
+        <thead>
+            <tr>
+                <th>{{Nom}}</th>
+                <th>{{Paramètres}}</th>
+                <th>{{Action}}</th>
+            </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+    </table>
+
+</div>
+</div>
 
 </div>
 </div>
